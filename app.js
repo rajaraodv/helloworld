54414375fc103596742bafe39a4a252ef31a2dc2
Raja Rao DV <rajar@vmware.com>
2012-03-25 12:24:32 -0700

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ var http = require('http');
 var port = process.env.VCAP_APP_PORT || '3000';
 var host = process.env.VCAP_APP_HOST || 'localhost';
 http.createServer(function(req, res){
-	res.end('hello world');
-	
+	res.end('hello world');	
 }).listen(port, host);
