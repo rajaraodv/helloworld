bafceffdc444f0b5e2ac93b73f3d9d184eaed1c3
Raja Rao DV <rajar@vmware.com>
2012-04-02 08:12:13 -0700

    minor change

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.VCAP_APP_PORT || '3000';
 var host = process.env.VCAP_APP_HOST || 'localhost';
 http.createServer(function(req, res){
-	res.end('hello world');	
+	res.end('Hello World! <br> I am running on..' + host +":"+ port);	
 }).listen(port, host);
